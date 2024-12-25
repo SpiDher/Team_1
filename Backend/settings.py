c89e1a21c70c305528b9809a717eadf052db8733
SpiDher <penivera655@gmail.com>
2024-12-25 21:54:43 +0100

    Changed folder structure

--- Backend/settings.py
+++ Backend/settings.py
@@ -119,7 +119,7 @@ STATIC_URL = 'static/'
 
 #File paths 
 MEDIA_URL= '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'learnify/media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
